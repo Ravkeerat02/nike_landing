54990490bd943c96bb38757c75471f74b6e2b9eb
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-10-29 00:16:50 -0400

    styling added

--- landing/src/App.jsx
+++ landing/src/App.jsx
@@ -1,9 +1,1 @@
-import { useState } from "react";
-
-import "./App.css";
-
-function App() {
-  return <h1 className="font-bold ">aef</h1>;
-}
-
-export default App;
+export default function App() {}
